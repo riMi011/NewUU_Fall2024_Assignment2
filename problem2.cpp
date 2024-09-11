a9044cf1e44a456bb637f3243937696cc1b746da
mir.rizoqulov <mir.rizoqulov@newuu.uz>
2024-09-11 11:45:23 +0500

    changed file name again changed

---
+++ problem2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int d1,d2;
+    cout<<"Enter diagonals for rhonbus: ";
+    cin>>d1>>d2;
+    cout<<"This is the `Area of rhombus:"<<d1*d2/2.0<<endl;
+    return 0;
+}
